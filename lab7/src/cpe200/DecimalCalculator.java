00d8114b82e199ac124db49493626164fb5422ed
Wiphob <wiphobkaoian@gmail.com>
2016-10-25 15:24:15 +0700

    580610677 Lab7-1 add function

--- lab7/src/cpe200/DecimalCalculator.java
+++ lab7/src/cpe200/DecimalCalculator.java
@@ -22,24 +22,43 @@ public class DecimalCalculator {
     }
 
     public String add() throws RuntimeException {
-        return null;
+        if(FOP.intValue() < 0 || SOP.intValue() < 0){
+            throw new RuntimeException("Operand is negative");
+        }
+        return FOP.add(SOP).stripTrailingZeros().toString();
     }
 
     public String subtract() throws RuntimeException {
-        return null;
+        if(FOP.intValue() < 0 || SOP.intValue() < 0){
+            throw new RuntimeException("Operand is negative");
+        }
+        return FOP.subtract(SOP).stripTrailingZeros().toString();
     }
 
     public String multiply() throws RuntimeException {
-        return null;
+        if(FOP.intValue() < 0 || SOP.intValue() < 0){
+            throw new RuntimeException("Operand is negative");
+        }
+        return FOP.multiply(SOP).stripTrailingZeros().toString();
     }
 
     /* This method should throw an exception when divide by zero */
     public String division() throws RuntimeException {
-        return null;
+        if(SOP.equals(0)) {
+            throw new ArithmeticException("ERROR");
+        }
+        else if(FOP.intValue() < 0 || SOP.intValue() < 0){
+            throw new RuntimeException("Operand is negative");
+        }
+        return FOP.divide(SOP,5,BigDecimal.ROUND_UP).stripTrailingZeros().toString();
     }
 
     public String power() throws RuntimeException {
-        return null;
+        if(FOP.intValue() < 0 || SOP.intValue() < 0){
+            throw new RuntimeException("Operand is negative");
+        }
+        return FOP.pow(SOP.intValue()).stripTrailingZeros().toString();
+    }
     }
 
 }
