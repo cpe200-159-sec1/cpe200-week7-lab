e5caa4ab24344245113dac2ed5e424448746eb55
ICQCQ <darunphop.p@gmail.com>
2016-10-27 18:57:43 +0700

    Fix general missing code

--- lab7/src/cpe200/BinaryCalculator.java
+++ lab7/src/cpe200/BinaryCalculator.java
@@ -1,37 +1,73 @@
 package cpe200;
 
-public class BinaryCalculator {
-    public IOperand firstOperand;
-    public IOperand secondOperand;
+import java.math.BigDecimal;
 
-    public BinaryCalculator() {
-    }
+public class BinaryCalculator {
+    private IOperand firstOperand;
+    private IOperand secondOperand;
 
     public void setFirstOperand(IOperand operand) {
-    }
+                firstOperand = operand;
+            }
 
     public void setSecondOperand(IOperand operand) {
+        secondOperand = operand;
     }
 
+
     public String add() throws RuntimeException {
-        return null;
+        BigDecimal d1 = new BigDecimal(firstOperand.getOperand());
+        BigDecimal d2 = new BigDecimal(secondOperand.getOperand());
+        if(d1.compareTo(BigDecimal.ZERO) < 0 || d2.compareTo(BigDecimal.ZERO) < 0){
+                throw  new  RuntimeException("operan < 1");
+            }
+
+                return d1.add(d2).stripTrailingZeros().toString();
     }
 
     public String subtract() throws RuntimeException {
-        return null;
+        BigDecimal d1 = new BigDecimal(firstOperand.getOperand());
+        BigDecimal d2 = new BigDecimal(secondOperand.getOperand());
+        if(d1.compareTo(BigDecimal.ZERO) < 0 || d2.compareTo(BigDecimal.ZERO) < 0){
+                throw  new  RuntimeException("operan < 1");
+            }
+
+                return d1.subtract(d2).stripTrailingZeros().toString();
     }
 
     public String multiply() throws RuntimeException {
-        return null;
+        BigDecimal d1 = new BigDecimal(firstOperand.getOperand());
+        BigDecimal d2 = new BigDecimal(secondOperand.getOperand());
+        if(d1.compareTo(BigDecimal.ZERO) < 0 || d2.compareTo(BigDecimal.ZERO) < 0){
+                throw  new  RuntimeException("operan < 1");
+            }
+
+                return d1.multiply(d2).stripTrailingZeros().toString();
     }
 
     /* This method should throw an exception when divide by zero */
     public String division() throws RuntimeException {
-        return null;
+        BigDecimal d1 = new BigDecimal(firstOperand.getOperand());
+        BigDecimal d2 = new BigDecimal(secondOperand.getOperand());
+        if(d1.compareTo(BigDecimal.ZERO) < 0 || d2.compareTo(BigDecimal.ZERO) < 0){
+                throw  new  RuntimeException("operan < 1");
+            }
+        if(d2.compareTo(BigDecimal.ZERO) == 0){
+                throw  new  ArithmeticException("operan < 1");
+            }
+
+                return d1.divide(d2,5,BigDecimal.ROUND_HALF_UP).stripTrailingZeros().toString();
     }
 
     public String power() throws RuntimeException {
-        return null;
-    }
+        BigDecimal d1 = new BigDecimal(firstOperand.getOperand());
+        BigDecimal d2 = new BigDecimal(secondOperand.getOperand());
+        if(d1.compareTo(BigDecimal.ZERO) < 0 || d2.compareTo(BigDecimal.ZERO) < 0){
+                throw  new  RuntimeException("operan < 1");
+            }
 
+                d1 = d1.pow(d2.intValue());
+        d1 = d1.stripTrailingZeros();
+        return d1.toString();
+    }
 }
--- lab7/src/cpe200Test/BinaryCalculatorTest.java
+++ lab7/src/cpe200Test/BinaryCalculatorTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 
+import static com.sun.xml.internal.ws.dump.LoggingDumpTube.Position.Before;
 import static org.junit.Assert.*;
 
 
