b1b5ac4639813a02013b55b64bfa29c165132045
kawewut <kawewut.chu@gmail.com>
2016-10-27 18:28:51 +0700

    LAB1

--- lab7/src/cpe200/decimalCalculator.java
+++ lab7/src/cpe200/decimalCalculator.java
@@ -1,7 +1,37 @@
 package cpe200;
 
 public class decimalCalculator extends BaseCalculator{
-    private IOperand firstOperand;
-    private IOperand secondOperand;
+    public IOperand firstOperand;
+    public IOperand secondOperand;
+
+    public decimalCalculator() {
+    }
+
+    public void setFirstOperand(IOperand operand) {
+    }
+
+    public void setSecondOperand(IOperand operand) {
+    }
+
+    public String add() throws RuntimeException {
+        return null;
+    }
+
+    public String subtract() throws RuntimeException {
+        return null;
+    }
+
+    public String multiply() throws RuntimeException {
+        return null;
+    }
+
+    /* This method should throw an exception when divide by zero */
+    public String division() throws RuntimeException {
+        return null;
+    }
+
+    public String power() throws RuntimeException {
+        return null;
+    }
 
 }
