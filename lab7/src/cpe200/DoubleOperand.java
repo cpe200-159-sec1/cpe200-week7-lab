92c91cfd278fcbda5952978d6d2d214662899614
omhsin007 <makoto.diva@gmail.com>
2016-10-27 18:55:48 +0700

    Copy file from lab 6

--- lab7/src/cpe200/DoubleOperand.java
+++ lab7/src/cpe200/DoubleOperand.java
@@ -1,16 +1,19 @@
 package cpe200;
 
+import java.math.BigDecimal;
+
 /**
  * Created by pruet on 26/9/2559.
  */
 public class DoubleOperand implements IOperand{
+    private String operand;
 
-    public DoubleOperand(Double d) {
-
+    public DoubleOperand(double operand) {
+        this.operand = Double.toString(operand);
     }
 
     @Override
     public String getOperand() {
-        return null;
+        return operand;
     }
 }
