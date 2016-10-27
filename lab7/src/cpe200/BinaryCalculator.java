95b8ef678ae12b2b1c3227bceb5ebdb1fe8c4f69
Chawasit Tengtrairatana <chawasit_teng@cmu.ac.th>
2016-10-27 16:50:44 +0700

    copy code from lab6

--- lab7/src/cpe200/BinaryCalculator.java
+++ lab7/src/cpe200/BinaryCalculator.java
@@ -1,37 +1,71 @@
 package cpe200;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+/**
+ * Created by pruet on 5/9/2559.
+ */
+
 public class BinaryCalculator {
-    public IOperand firstOperand;
-    public IOperand secondOperand;
+    private BigDecimal firstOperand;
+    private BigDecimal secondOperand;
 
     public BinaryCalculator() {
+        firstOperand = BigDecimal.ZERO;
+        secondOperand = BigDecimal.ZERO;
     }
 
     public void setFirstOperand(IOperand operand) {
+        firstOperand = new BigDecimal(operand.getOperand());
     }
+
 
     public void setSecondOperand(IOperand operand) {
+        secondOperand = new BigDecimal(operand.getOperand());
     }
 
-    public String add() throws RuntimeException {
-        return null;
+    public String add() {
+        CheckNegativeOperand();
+        return firstOperand.add(secondOperand)
+                .stripTrailingZeros().toString();
     }
 
-    public String subtract() throws RuntimeException {
-        return null;
+    public String subtract() {
+        CheckNegativeOperand();
+        return firstOperand.subtract(secondOperand)
+                .stripTrailingZeros().toString();
     }
 
-    public String multiply() throws RuntimeException {
-        return null;
+    public String multiply() {
+        CheckNegativeOperand();
+        return firstOperand.multiply(secondOperand)
+                .stripTrailingZeros().toString();
     }
 
     /* This method should throw an exception when divide by zero */
-    public String division() throws RuntimeException {
-        return null;
+    public String division() {
+        if (secondOperand.equals(BigDecimal.ZERO))
+            throw new ArithmeticException();
+        CheckNegativeOperand();
+        return firstOperand.divide(secondOperand, 5, RoundingMode.HALF_UP)
+                .stripTrailingZeros().toString();
     }
 
-    public String power() throws RuntimeException {
-        return null;
+
+    public String power() {
+        CheckNegativeOperand();
+        return BigDecimal.valueOf(
+                Math.pow(
+                        firstOperand.doubleValue(),
+                        secondOperand.doubleValue()
+                )
+        ).stripTrailingZeros().toString();
     }
 
+    private void CheckNegativeOperand() {
+        if (firstOperand.compareTo(BigDecimal.ZERO) < 0 || secondOperand.compareTo(BigDecimal.ZERO) < 0) {
+            throw new RuntimeException("Operand must greater than ZERO");
+        }
+    }
 }
