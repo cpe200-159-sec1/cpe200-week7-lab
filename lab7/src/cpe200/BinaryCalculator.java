bf23f1a7202471d716b59df0ff462a4761f8084b
oraya1996 <jjanemark2345@gmail.com>
2016-11-01 00:10:30 +0700

    lab7.3 Generalize DecimalCalculator and BinaryCalculator

--- lab7/src/cpe200/BinaryCalculator.java
+++ lab7/src/cpe200/BinaryCalculator.java
@@ -6,7 +6,7 @@ public class BinaryCalculator extends BaseCalculator {
 
 
     public void setFirstOperand(IOperand operand) {
-        if(operand.getOperand().matches("^[01]+$")){
+        if(operand.getOperand().matches("^[01+]")){
             super.firstOperand = new IntegerOperand(Integer.parseInt(operand.getOperand(),2));
         }else{
             throw new ArithmeticException("The input isn't binary format.");
@@ -14,16 +14,13 @@ public class BinaryCalculator extends BaseCalculator {
     }
 
     public void setSecondOperand(IOperand operand) {
-        if(operand.getOperand().matches("^[01]+$")){
+        if(operand.getOperand().matches("^[01+]")){
             super.secondOperand = new IntegerOperand(Integer.parseInt(operand.getOperand(),2));
         }else{
             throw new ArithmeticException("The input isn't binary format.");
         }
     }
-    @Override
-    public String getAnswer(){
-        return Integer.toBinaryString(Integer.parseInt(super.answer));
-    }
+
 
 
 }
