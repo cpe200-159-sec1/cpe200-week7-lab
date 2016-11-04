e892d3e0050ebd50f2046c2b176f807867dd5481
Khimmy <annani12@hotmail.com>
2016-11-04 21:15:26 +0700

    BaseCalculator

--- lab7/src/cpe200/BaseCalculator.java
+++ lab7/src/cpe200/BaseCalculator.java
@@ -4,106 +4,72 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 /**
- * Created by Khimmy on 11/2/2016.
+ * Created by Khimmy on 11/4/2016.
  */
-public class BinaryCalculator extends BaseCalculator {
+public class BaseCalculator {
     private BigDecimal firstOperand;
     private BigDecimal secondOperand;
 
-    public BinaryCalculator() {
-        firstOperand = new BigDecimal(0);
-        secondOperand = new BigDecimal(0);
+
+    public BaseCalculator() {
     }
 
-    @Override
     public void setFirstOperand(IOperand operand) {
-        firstOperand = BinToDec(operand.getOperand(), firstOperand);
+        firstOperand = new BigDecimal(operand.getOperand());
     }
 
-    @Override
     public void setSecondOperand(IOperand operand) {
-        secondOperand = BinToDec(operand.getOperand(), secondOperand);
+        secondOperand = new BigDecimal(operand.getOperand());
     }
 
-    private BigDecimal BinToDec(String k, BigDecimal x) {
-        int power = -1;
-        for (int i = k.length() - 1; i >= 0; i--) {
-            power++;
-            if (k.charAt(i) == '0')
-                continue;
-            else if (k.charAt(i) == '1')
-                x = x.add(new BigDecimal(Double.toString(Math.pow(2, power))));
-            else
-                throw new ArithmeticException("Error");
-        }
-        return x;
-    }
-
-    private String DecToBin(int x) {
-        String ans = "";
-
-        while (x != 0) {
-            ans = Integer.toString(x % 2) + ans;
-            x /= 2;
-        }
-        if (ans == "") ans = "0";
-
-        return ans;
-    }
-
-    @Override
     public String add() throws RuntimeException {
+
         if (firstOperand.doubleValue() < 0 || secondOperand.doubleValue() < 0) {
             throw new RuntimeException("ERROR");
         } else {
-            return DecToBin(firstOperand.add(secondOperand).stripTrailingZeros().intValue());
+
+            return firstOperand.add(secondOperand).stripTrailingZeros().toString();
         }
     }
 
-    @Override
     public String subtract() throws RuntimeException {
 
         if (firstOperand.doubleValue() < 0 || secondOperand.doubleValue() < 0) {
             throw new RuntimeException("ERROR");
         } else {
 
-            return DecToBin(firstOperand.subtract(secondOperand).stripTrailingZeros().intValue());
+            return firstOperand.subtract(secondOperand).stripTrailingZeros().toString();
         }
 
     }
 
-    @Override
     public String multiply() throws RuntimeException {
         if (firstOperand.doubleValue() < 0 || secondOperand.doubleValue() < 0) {
             throw new RuntimeException("ERROR");
         } else {
 
-            return DecToBin(firstOperand.multiply(secondOperand).stripTrailingZeros().intValue());
+            return firstOperand.multiply(secondOperand).stripTrailingZeros().toString();
         }
     }
 
     /* This method should throw an exception when divide by zero */
-    @Override
     public String division() throws RuntimeException {
         if (firstOperand.doubleValue() < 0 || secondOperand.doubleValue() < 0) {
             throw new RuntimeException("ERROR");
         } else if (secondOperand.intValue() == 0) {
             throw new ArithmeticException("devide by zero");
         }
-        return DecToBin(firstOperand.divide(secondOperand, 5, RoundingMode.HALF_UP).stripTrailingZeros().intValue());
+        return firstOperand.divide(secondOperand, 5, RoundingMode.HALF_UP).stripTrailingZeros().toString();
     }
 
-    @Override
     public String power() throws RuntimeException {
         if (firstOperand.doubleValue() < 0 || secondOperand.doubleValue() < 0) {
             throw new RuntimeException("ERROR");
         } else {
             String ans = Double.toString(Math.pow(firstOperand.doubleValue(), secondOperand.doubleValue()));
             firstOperand = new BigDecimal(ans);
-            return DecToBin(firstOperand.stripTrailingZeros().intValue());
+            return firstOperand.stripTrailingZeros().toString();
         }
 
     }
-
 }
-
