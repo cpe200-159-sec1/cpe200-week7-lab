ef1bd971461d18ed189e4f4698719638d8f21589
Potsawat <potsawat_3232@hotmail.com>
2016-11-04 16:15:50 +0700

    lab 3 done  -BaseCalculator

--- lab7/src/cpe200/BaseCalculator.java
+++ lab7/src/cpe200/BaseCalculator.java
@@ -3,80 +3,52 @@ package cpe200;
 import java.math.BigDecimal;
 
 /**
- * Created by User on 2/11/2559.
+ * Created by User on 4/11/2559.
  */
-public class BinaryCalculator extends BaseCalculator {
+public class BaseCalculator {
     private BigDecimal firstOperand;
     private BigDecimal secondOperand;
 
-    @Override 
+    public BaseCalculator() {
+    }
+
     public void setFirstOperand(IOperand operand) {
-        firstOperand = new BigDecimal(0);
-        firstOperand = BinToDec(operand.getOperand(), firstOperand);
+        firstOperand = new BigDecimal(operand.getOperand());
     }
 
-    @Override
+
     public void setSecondOperand(IOperand operand) {
-        secondOperand = new BigDecimal(0);
-        secondOperand = BinToDec(operand.getOperand(), secondOperand);
+        secondOperand = new BigDecimal(operand.getOperand());
     }
 
-    private BigDecimal BinToDec(String check, BigDecimal x) {
-        int power = -1;
-        for (int i = check.length() - 1; i >= 0; i--) {
-            power++;
-            if (check.charAt(i) == '0')
-                continue;
-            else if (check.charAt(i) == '1')
-                x = x.add(new BigDecimal(Double.toString(Math.pow(2, power))));
-            else
-                throw new ArithmeticException("Error");
-        }
-        return x;
-    }
-
-    private String DecToBin(int x) {
-        String ans = "";
-        while (x != 0) {
-            ans = Integer.toString(x % 2) + ans;
-            x /= 2;
-        }
-        if (ans == "") ans = "0";
-        return ans;
-    }
-
-    @Override
     public String add() {
         if (firstOperand.doubleValue() < 0 || secondOperand.doubleValue() < 0)
             throw new RuntimeException("Error");
         BigDecimal x = firstOperand.add(secondOperand);
         x = x.setScale(5, BigDecimal.ROUND_HALF_UP).stripTrailingZeros();
 
-        return DecToBin(x.intValue());
+        return x.toString();
     }
 
-    @Override
     public String subtract() {
         if (firstOperand.doubleValue() < 0 || secondOperand.doubleValue() < 0)
             throw new RuntimeException("Error");
         BigDecimal x = firstOperand.subtract(secondOperand);
         x = x.setScale(5, BigDecimal.ROUND_HALF_UP).stripTrailingZeros();
 
-        return DecToBin(x.intValue());
+        return x.toString();
     }
 
-    @Override
     public String multiply() {
         if (firstOperand.doubleValue() < 0 || secondOperand.doubleValue() < 0)
             throw new RuntimeException("Error");
         BigDecimal x = firstOperand.multiply(secondOperand);
         x = x.setScale(5, BigDecimal.ROUND_HALF_UP).stripTrailingZeros();
 
-        return DecToBin(x.intValue());
+        return x.toString();
     }
 
     /* This method should throw an exception when divide by zero */
-    @Override
     public String division() {
         if (firstOperand.doubleValue() < 0 || secondOperand.doubleValue() < 0)
             throw new RuntimeException("Error");
@@ -84,10 +56,9 @@ public class BinaryCalculator extends BaseCalculator {
             throw new ArithmeticException("Error");
         BigDecimal x = firstOperand.divide(secondOperand, 5, BigDecimal.ROUND_HALF_UP).stripTrailingZeros();
 
-        return DecToBin(x.intValue());
+        return x.toString();
     }
 
-    @Override
     public String power() {
         if (firstOperand.doubleValue() < 0 || secondOperand.doubleValue() < 0)
             throw new RuntimeException("Error");
@@ -97,6 +68,6 @@ public class BinaryCalculator extends BaseCalculator {
                 )
         );
         x = x.setScale(5, BigDecimal.ROUND_HALF_UP).stripTrailingZeros();
-        return DecToBin(x.intValue());
+        return x.toString();
     }
 }
