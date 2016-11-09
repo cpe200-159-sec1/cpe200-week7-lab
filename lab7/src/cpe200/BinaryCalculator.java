9428661238a1e107e2f4a64c755cc7b34789c848
chalitphol kuensuwan <chalitphol@gmail.com>
2016-11-09 20:19:45 +0700

    580610630 lab7 finish

--- lab7/src/cpe200/BinaryCalculator.java
+++ lab7/src/cpe200/BinaryCalculator.java
@@ -1,37 +1,32 @@
 package cpe200;
 
-public class BinaryCalculator {
-    public IOperand firstOperand;
-    public IOperand secondOperand;
+import sun.security.x509.IPAddressName;
 
-    public BinaryCalculator() {
-    }
+/**
+ * Created by MSI GP72 on 3/11/2559.
+ */
+public class BinaryCalculator extends BaseCalculator{
 
+    @Override
     public void setFirstOperand(IOperand operand) {
+        if(operand.getOperand().matches("^[01]+$")){
+            super.firstOperand = new IntegerOperand(Integer.parseInt(operand.getOperand(),2));
+        }else{
+            throw new RuntimeException("The input isn't binary format.");
+        }
     }
 
+    @Override
     public void setSecondOperand(IOperand operand) {
+        if(operand.getOperand().matches("^[01]+$")){
+            super.secondOperand = new IntegerOperand(Integer.parseInt(operand.getOperand(),2));
+        }else{
+            throw new RuntimeException("The input isn't binary format.");
+        }
     }
 
-    public String add() throws RuntimeException {
-        return null;
+    @Override
+    public String getAnswer(){
+        return Integer.toBinaryString(Integer.parseInt(super.answer));
     }
-
-    public String subtract() throws RuntimeException {
-        return null;
-    }
-
-    public String multiply() throws RuntimeException {
-        return null;
-    }
-
-    /* This method should throw an exception when divide by zero */
-    public String division() throws RuntimeException {
-        return null;
-    }
-
-    public String power() throws RuntimeException {
-        return null;
-    }
-
 }
