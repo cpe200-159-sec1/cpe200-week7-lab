81dbfd6f4dc00480716dfbdc14f95bf53492fa96
Sharof <pro21808@gmail.com>
2016-11-15 18:49:22 +0700

    lab7.1

---
+++ lab7/src/cpe200/DecimalCalculator.java
@@ -0,0 +1,58 @@
+package cpe200;
+
+import java.math.BigDecimal;
+
+public class DecimalCalculator {
+    private BigDecimal firstOperand;
+    private BigDecimal secondOperand;
+
+    public DecimalCalculator() {
+        firstOperand = new BigDecimal(0);
+        secondOperand = new BigDecimal(0);
+    }
+
+    public void setFirstOperand(IOperand operand) {
+        firstOperand = new BigDecimal(operand.getOperand());
+    }
+
+    public void setSecondOperand(IOperand operand) {
+        secondOperand = new BigDecimal(operand.getOperand());
+    }
+
+    public String add() throws RuntimeException {
+        if (firstOperand.compareTo(BigDecimal.ZERO) <= 0 || secondOperand.compareTo(BigDecimal.ZERO) <= 0){
+            throw new RuntimeException("non-nagative");
+        }
+        return firstOperand.add(secondOperand).stripTrailingZeros().toString();
+    }
+
+    public String subtract() throws RuntimeException {
+        if (firstOperand.compareTo(BigDecimal.ZERO) <= 0 || secondOperand.compareTo(BigDecimal.ZERO) <= 0){
+            throw new RuntimeException("non-nagative");
+        }
+        return firstOperand.subtract(secondOperand).stripTrailingZeros().toString();
+    }
+
+    public String multiply() throws RuntimeException {
+        if (firstOperand.compareTo(BigDecimal.ZERO) <= 0 || secondOperand.compareTo(BigDecimal.ZERO) <=0){
+            throw new RuntimeException("non-negative");
+        }
+        return firstOperand.multiply(secondOperand).stripTrailingZeros().toString();
+    }
+
+    /* This method should throw an exception when divide by zero */
+    public String division() throws RuntimeException {
+        if (firstOperand.compareTo(BigDecimal.ZERO) <= 0 || secondOperand.compareTo(BigDecimal.ZERO) <= 0){
+            throw new ArithmeticException("non-negative");
+        }
+        return firstOperand.divide(secondOperand,5,BigDecimal.ROUND_UP).stripTrailingZeros().toString();
+    }
+
+    public String power() throws RuntimeException {
+        if (firstOperand.compareTo(BigDecimal.ZERO) <= 0 || secondOperand.compareTo(BigDecimal.ZERO) <= 0){
+            throw new ArithmeticException("non-negative");
+        }
+        return BigDecimal.valueOf(Math.pow(firstOperand.doubleValue(),secondOperand.doubleValue())).stripTrailingZeros().toString();
+    }
+
+}
