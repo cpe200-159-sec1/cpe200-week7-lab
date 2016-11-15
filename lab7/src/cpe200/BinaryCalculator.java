3c152cd7db070174427e9f4856609e6a81b95fb1
supitch <sudukutoto@hotmail.com>
2016-11-15 12:07:31 +0700

    ลืมกำ

--- lab7/src/cpe200/BinaryCalculator.java
+++ lab7/src/cpe200/BinaryCalculator.java
@@ -1,37 +1,76 @@
 package cpe200;
 
-public class BinaryCalculator {
+/**
+ * Created by Nickky on 9/11/2559.
+ */
+public class BinaryCalculator extends BaseCalculator {
     public IOperand firstOperand;
     public IOperand secondOperand;
 
     public BinaryCalculator() {
     }
+    private int binaryToInt(String operand){
+        return Integer.parseInt(operand,2);
+    }
 
     public void setFirstOperand(IOperand operand) {
+        for (int i=0; i<operand.getOperand().length(); i++){
+            if (operand.getOperand().charAt(i) != '0'){
+                if (operand.getOperand().charAt(i) != '1'){
+                    throw new ArithmeticException();
+                }
+            }
+        }
+        firstOperand = operand;
     }
 
     public void setSecondOperand(IOperand operand) {
+        for (int i=0; i<operand.getOperand().length(); i++){
+            if (operand.getOperand().charAt(i) != '0'){
+                if (operand.getOperand().charAt(i) != '1'){
+                    throw new ArithmeticException();
+                }
+            }
+        }
+        secondOperand = operand;
     }
 
     public String add() throws RuntimeException {
-        return null;
+        int first, second;
+        first = binaryToInt(firstOperand.getOperand());
+        second = binaryToInt(secondOperand.getOperand());
+        return Integer.toBinaryString(first + second);
     }
 
     public String subtract() throws RuntimeException {
-        return null;
+        int first, second;
+        first = binaryToInt(firstOperand.getOperand());
+        second = binaryToInt(secondOperand.getOperand());
+        return Integer.toBinaryString(first - second);
     }
 
     public String multiply() throws RuntimeException {
-        return null;
+        int first, second;
+        first = binaryToInt(firstOperand.getOperand());
+        second = binaryToInt(secondOperand.getOperand());
+        return Integer.toBinaryString(first * second);
     }
 
     /* This method should throw an exception when divide by zero */
     public String division() throws RuntimeException {
-        return null;
+        int first, second;
+        first = binaryToInt(firstOperand.getOperand());
+        second = binaryToInt(secondOperand.getOperand());
+        return Integer.toBinaryString(first / second);
     }
 
     public String power() throws RuntimeException {
-        return null;
+        int first, second;
+        first = binaryToInt(firstOperand.getOperand());
+        second = binaryToInt(secondOperand.getOperand());
+        return Integer.toBinaryString((int)Math.pow((double) first, (double) second));
     }
+
+
 
 }
